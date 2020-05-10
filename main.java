570e1a1047642e66d3e524f2147a845344e98e56
aRodriguez96 <albertrodriguez@Alberts-MacBook-Air.local>
2020-05-09 21:19:58 -0400

    added boyer moore

--- main.java
+++ main.java
@@ -1,1 +1,9 @@
-
+class main{ 
+      
+    public static void main(String []args) { 
+          //BOYER MOORE
+         char txt[] = "ABAAABCD".toCharArray(); 
+         char pat[] = "ABC".toCharArray(); 
+         BM.search(txt, pat); 
+    } 
+}  
