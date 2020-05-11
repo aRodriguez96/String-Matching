061b7f85c98f35419df573ad1067499a56c1d350
Adam Carballido <carballido.adam@yahoo.com>
2020-05-11 14:33:53 -0400

    Added KNP algo

--- main.java
+++ main.java
@@ -4,6 +4,10 @@ class main{
           //BOYER MOORE
          char txt[] = "ABAAABCD".toCharArray(); 
          char pat[] = "ABC".toCharArray(); 
+         String kmptxt = "ABAAABCD";
+         String kmppat = "ABC";
+         
          BM.search(txt, pat); 
+         KMP.KMPSearch(kmppat, kmptxt);
     } 
 }  
